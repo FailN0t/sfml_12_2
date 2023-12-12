80384067899629b4976cbed798b1a9f005dd2648
Igor Putyajkin <putyajkin99@gmail.com>
2023-12-12 21:10:39 +0300

    init13

--- sfml_12_2/sfml_12_2.cpp
+++ sfml_12_2/sfml_12_2.cpp
@@ -2,15 +2,41 @@
 #include <iostream>
 using namespace sf;
 void circle(Shape& shape) {
-	shape.setPosition(shape.getPosition().x - 20, shape.getPosition().y - 20);
+	shape.setPosition(shape.getPosition().x + 4, shape.getPosition().y + 4);
+}
+void circleMgnum(CircleShape& shape, CircleShape& shape2, RenderWindow& window) {
+	if (shape.getPosition().x <= 0) {
+		shape2.setPosition((float)window.getSize().x + shape.getPosition().x, shape2.getPosition().y);
+		if (shape.getPosition().x < (1 - (float)window.getSize().x)) {
+			shape.setPosition(0, shape.getPosition().y);
+		}
+	}
+	if (shape.getPosition().y <= 0) {
+		shape2.setPosition(shape2.getPosition().x, (float)window.getSize().y + shape.getPosition().y);
+		if (shape.getPosition().y < (1 - (float)window.getSize().y)) {
+			shape.setPosition(shape.getPosition().x, 0);
+		}
+	}
+	if (shape.getPosition().x + shape.getRadius() * 2 >= window.getSize().x) {
+		shape2.setPosition(-(float)window.getSize().x + shape.getPosition().x, shape2.getPosition().y);
+		if (shape.getPosition().x + shape.getRadius() * 2 - (float)window.getSize().x > window.getSize().x) {
+			shape.setPosition(-shape.getRadius() * 2, shape.getPosition().y);
+		}
+	}
+	if (shape.getPosition().y + shape.getRadius() * 2 >= window.getSize().y) {
+		shape2.setPosition(shape2.getPosition().x, -(float)window.getSize().y + shape.getPosition().y);
+		if (shape.getPosition().y + shape.getRadius() * 2 - (float)window.getSize().x > window.getSize().y) {
+			shape.setPosition(shape.getPosition().x, -shape.getRadius() * 2);
+		}
+	}
 }
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(400, 400), "Lesson 2. kychka-pc.ru");
-	window.setFramerateLimit(5);
+	window.setFramerateLimit(30);
 	sf::CircleShape shape(100.f);
-	CircleShape shape2;
+	
 	//shape.setFillColor(sf::Color::Red);
 
 	Texture textureFont;
@@ -22,6 +48,7 @@ int main()
 	Texture textureCircle;
 	textureCircle.loadFromFile("res/circle.jpg");
 	shape.setTexture(&textureCircle);
+	CircleShape shape2;
 	shape2 = shape;
 	shape2.setPosition((Vector2f)window.getSize());
 	int x = 0, y = 0;
@@ -33,31 +60,8 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		if (shape.getPosition().x <= 0) {
-			shape2.setPosition((float)window.getSize().x + shape.getPosition().x, shape2.getPosition().y);
-			if (shape.getPosition().x < (1-(float)window.getSize().x)) {
-				shape.setPosition(0, shape.getPosition().y);
-			}
-		}
-		if (shape.getPosition().y <= 0) {
-			shape2.setPosition(shape2.getPosition().x, (float)window.getSize().y + shape.getPosition().y);
-			if (shape.getPosition().y < (1 - (float)window.getSize().y)) {
-				shape.setPosition(shape.getPosition().x, 0);
-			}
-		}
-		if (shape.getPosition().x + shape.getRadius()*2 >= window.getSize().x) {
-			shape2.setPosition(-(float)window.getSize().x + shape.getPosition().x, shape2.getPosition().y);
-			if (shape.getPosition().x  >= ) {
-				shape.setPosition(0, shape.getPosition().y);
-			}
-		}
-		if (shape.getPosition().y + shape.getRadius() * 2 >= window.getSize().y) {
-			shape2.setPosition(shape2.getPosition().x, -(float)window.getSize().y + shape.getPosition().y /*- (2 * shape.getRadius())*/);
-			/*if (shape.getPosition().y < -399) {
-				shape.setPosition(shape.getPosition().x, 0);
-			}*/
-		}
-		std::cout << shape.getPosition().y << std::endl;
+		
+		circleMgnum(shape, shape2, window);
 		circle(shape);
 		window.clear();
 		window.draw(recShape);
